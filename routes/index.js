7f0c962b664d96129f4689d467f5b1a6e541e849
Jean-Baptiste Avella <jeanbaptiste.avella@gmail.com>
2022-04-26 13:54:00 +0200

    delete function works !

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 const userController = require("../controllers/User");
 var router = express.Router();
 const { checkAuth } = require("../middlewares/checkAuth");
-
+const bookModel = require("../models/bookModel");
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("Salut");
@@ -14,7 +14,19 @@ router.get("/register", userController.getUsersFromDB);
 
 router.post("/add-book", checkAuth, userController.saveBookToDB);
 
-router.delete("/:id", checkAuth, userController.deleteBookToDB);
+router.delete("/:id", checkAuth, (req, res) => {
+  console.log(req.params.id);
+
+  bookModel
+    .findByIdAndDelete(req.params.id)
+    .then(() => {
+      console.log("Succesfully delete to DB");
+      res.send("Client bien supprimé en DB");
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
+});
 
 router.get("/bibliotheque", checkAuth, userController.getUserBooks);
 
